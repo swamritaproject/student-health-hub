ea448d403f19dff482c6b004c9a630f18b307f88
swamritaproject <swamrita.project@gmail.com>
2026-07-04 00:57:30 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function bmi(){
+let h=+hEl().value/100,w=+wEl().value;
+if(!h||!w)return alert("Enter height & weight");
+let b=w/(h*h),txt="";
+if(b<18.5){txt="Underweight";set(25,"#ff9800")}
+else if(b<25){txt="Normal";set(50,"#4caf50")}
+else if(b<30){txt="Overweight";set(75,"#ffc107")}
+else{txt="Obese";set(100,"#f44336")}
+bmiOut.innerHTML=`BMI: ${b.toFixed(1)} - ${txt}`;
+}
+function set(v,c){fill.style.width=v+"%";fill.style.background=c}
+function hEl(){return document.getElementById("h")}
+function wEl(){return document.getElementById("w")}
+const bmiOut=document.getElementById("bmiOut"),fill=document.getElementById("fill");
+function water(){let w=+ww.value;if(!w)return alert("Enter weight");waterOut.innerHTML=`Drink ${(w*35/1000).toFixed(1)} litres/day`; }
+function sleepRec(){sleepOut.innerHTML={child:"9-12 hours",teen:"8-10 hours",adult:"7-9 hours"}[age.value]}
+function calories(){
+let a=+ca.value,w=+cw.value,h=+ch.value;
+if(!a||!w||!h)return alert("Fill all fields");
+let b=(g.value=="Male")?(10*w+6.25*h-5*a+5):(10*w+6.25*h-5*a-161);
+calOut.innerHTML=`Estimated Calories: ${Math.round(b)} kcal/day`;
+}
