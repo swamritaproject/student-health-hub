5dbef86a420c24519acad3f9d2d82e55a4173063
swamritaproject <swamrita.project@gmail.com>
2026-07-13 00:15:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -217,47 +217,4 @@ break;
 
 result.innerHTML=html;
 
-    }years--;
-months+=12;
-}
-
-document.getElementById("ageResult").innerHTML=
-`Age : ${years} Years ${months} Months ${days} Days`;
-
-}
-function bmi(){
-let h=+hEl().value/100,w=+wEl().value;
-if(!h||!w)return alert("Enter height & weight");
-let b=w/(h*h),txt="";
-if(b<18.5){txt="Underweight";set(25,"#ff9800")}
-else if(b<25){txt="Normal";set(50,"#4caf50")}
-else if(b<30){txt="Overweight";set(75,"#ffc107")}
-else{txt="Obese";set(100,"#f44336")}
-bmiOut.innerHTML=`BMI: ${b.toFixed(1)} - ${txt}`;
-}
-function set(v,c){fill.style.width=v+"%";fill.style.background=c}
-function hEl(){return document.getElementById("h")}
-function wEl(){return document.getElementById("w")}
-const bmiOut=document.getElementById("bmiOut"),fill=document.getElementById("fill");
-function water(){let w=+ww.value;if(!w)return alert("Enter weight");waterOut.innerHTML=`Drink atleast ${(w*35/1000).toFixed(1)} litres/day`; }
-function sleepRec(){
-
-    const ageGroup = document.getElementById("sleepAge").value;
-    const sleepOut = document.getElementById("sleepOut");
-
-    if(ageGroup=="child"){
-        sleepOut.innerHTML="Recommended Sleep: 9–12 Hours";
-    }
-    else if(ageGroup=="teen"){
-        sleepOut.innerHTML="Recommended Sleep: 8–10 Hours";
-    }
-    else{
-        sleepOut.innerHTML="Recommended Sleep: 7–9 Hours";
-    }
-}
-function calories(){
-let a=+ca.value,w=+cw.value,h=+ch.value;
-if(!a||!w||!h)return alert("Fill all fields");
-let b=(g.value=="Male")?(10*w+6.25*h-5*a+5):(10*w+6.25*h-5*a-161);
-calOut.innerHTML=`Estimated Calories: ${Math.round(b)} kcal/day`;
 }
